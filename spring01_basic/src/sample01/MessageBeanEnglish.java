2ac998f76546ed4f129c7bcea8c61ddcd0ef953e
KIM-YOHAN <jkim6115@gmail.com>
2022-11-15 21:50:34 +0900

    20221115 | add

---
+++ spring01_basic/src/sample01/MessageBeanEnglish.java
@@ -0,0 +1,11 @@
+package sample01;
+
+public class MessageBeanEnglish {
+	public MessageBeanEnglish() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void sayHello(String name) {
+		System.out.printf("Hello, %s !!!", name);
+	}
+}
