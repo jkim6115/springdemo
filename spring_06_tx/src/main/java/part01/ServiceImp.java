8c0c67271c4980521ff4368b65bebde4329fdc1d
KIM-YOHAN <jkim6115@gmail.com>
2022-11-21 21:43:50 +0900

    20221121 | update

--- spring_06_tx/src/main/java/part01/ServiceImp.java
+++ spring_06_tx/src/main/java/part01/ServiceImp.java
@@ -1,10 +1,15 @@
 package part01;
 
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
+
 import model.MemDAO;
 import model.MemDTO;
 
 public class ServiceImp implements Service {
 	private MemDAO memDAO;
+	private TransactionTemplate transactionTemplate;
 	
 	public ServiceImp() {
 	}
@@ -12,10 +17,27 @@ public class ServiceImp implements Service {
 	public void setMemDAO(MemDAO memDAO) {
 		this.memDAO = memDAO;
 	}
+	
+	public void setTransactionTemplate(TransactionTemplate transactionTemplate) {
+		this.transactionTemplate = transactionTemplate;
+	}
 
 	@Override
 	public void insertProcess() {
-		memDAO.insertMethod(new MemDTO(41, "용팔이", 50, "경기"));
-		memDAO.insertMethod(new MemDTO(42, "유대위", 20, "대전"));
+		String result = transactionTemplate.execute(new TransactionCallback<String>() {
+			@Override
+			public String doInTransaction(TransactionStatus status) {
+				try {
+					memDAO.insertMethod(new MemDTO(44, "용팔이", 50, "경기"));
+					memDAO.insertMethod(new MemDTO(45, "유대위", 20, "대전"));
+					return "OK";
+				} catch (Exception e) {
+					status.setRollbackOnly();
+					return e.toString();
+				}				
+			}
+		});
+		
+		System.out.println("result: " + result);
 	}
 }
---
+++ spring_08_board/src/main/java/controller/BoardController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+
+import dto.PageDTO;
+import service.BoardService;
+
+@Controller
+public class BoardController {
+	
+	private BoardService service;
+	private PageDTO pdto;
+	private int currentPage;
+	
+	public BoardController() {
+	}
+}
