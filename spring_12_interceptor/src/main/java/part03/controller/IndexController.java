75ee9c6177acf917d84ef47b702a7da84cc13195
KIM-YOHAN <jkim6115@gmail.com>
2022-11-28 21:46:15 +0900

    20221128 | Update

---
+++ spring_12_interceptor/src/main/java/part03/controller/IndexController.java
@@ -0,0 +1,15 @@
+package part03.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	public IndexController() {
+	}
+	
+	@RequestMapping("/index.do")
+	public String process() {
+		return "part03/index";
+	}
+}
